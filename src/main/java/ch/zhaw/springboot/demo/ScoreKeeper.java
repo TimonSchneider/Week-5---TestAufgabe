83832c135d3f61bb83bab0bb4e388671ed568049
timonschneider <timon.schneider@hotmail.com>
2023-05-24 10:16:26 +0200

    Erstellen der Klasse ScoreKeeper

---
+++ src/main/java/ch/zhaw/springboot/demo/ScoreKeeper.java
@@ -0,0 +1,11 @@
+package ch.zhaw.springboot.demo;
+
+public class ScoreKeeper {
+    private int scoreTeamA;
+    private int scoreTeamB;
+
+    public ScoreKeeper() {
+        this.scoreTeamA = 0;
+        this.scoreTeamB = 0;
+    }
+}
